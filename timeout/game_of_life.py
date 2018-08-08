b4115a1528c12e0f752eeac3a2100d32e063a281
LenaAn <elena-anyusheva@yandex.ru>
2018-08-08 15:15:40 +0100

    aa

--- timeout/game_of_life.py
+++ timeout/game_of_life.py
@@ -1,3 +1,5 @@
+from itertools import product
+
 class Game:
 	def __init__(self, size=10):
 		self.size = size
@@ -11,4 +13,12 @@ class Game:
 		return self.grid[i][j] == 1
 
 
-		
+	def count_neigh(self, i, j):
+		count = 0
+		for di, dj in product([-1, 0, 1], [-1, 0, 1]):
+			if di == 0 and dj == 0:
+				pass
+			else:
+				count += self.grid[(i+di+self.size) % self.size][(j+dj+self.size) % self.size]
+		return count
+
--- timeout/test.py
+++ timeout/test.py
@@ -6,27 +6,39 @@ PATTERN = [[0, 0], [1, 1], [3, 5]]
 
 class TestGame(unittest.TestCase):
 
-    def test_size(self):
-    	g = Game(10)
-    	self.assertEqual(g.size, 10)
-    	self.assertEqual(len(g.grid), 10)
-    	self.assertEqual(len(g.grid[0]), 10)
+	def test_size(self):
+		g = Game(10)
+		self.assertEqual(g.size, 10)
+		self.assertEqual(len(g.grid), 10)
+		self.assertEqual(len(g.grid[0]), 10)
 
 
-    	g = Game(20)
-    	self.assertEqual(g.size, 20)
-    	self.assertEqual(len(g.grid), 20)
-    	self.assertEqual(len(g.grid[0]), 20)
+		g = Game(20)
+		self.assertEqual(g.size, 20)
+		self.assertEqual(len(g.grid), 20)
+		self.assertEqual(len(g.grid[0]), 20)
 
-    def test_set_active(self):
-    	g = Game(10)
-    	g.set_active(PATTERN)
+	def test_set_active(self):
+		g = Game(10)
+		g.set_active(PATTERN)
 
-    	for i in range(10):
-    		for j in range(10):
-    			if [i, j] in PATTERN:
-	    			self.assertEqual(g.isActive(i, j), 1)
+		for i in range(10):
+			for j in range(10):
+				if [i, j] in PATTERN:
+					self.assertEqual(g.isActive(i, j), 1)
+
+	def test_grid(self):
+		g = Game(10)
+		g.set_active(PATTERN)
+
+		for i in range(10):
+			for j in range(10):
+				if [i, j] in PATTERN:
+					self.assertEqual(g.isActive(i, j), 1)
+				else:
+					self.assertEqual(g.isActive(i, j), 0)
+
 
 
 if __name__ == '__main__':
-    unittest.main()
+	unittest.main()
